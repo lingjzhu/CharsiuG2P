ec425111b504fcd7667f950ddef8040f464d54d2
lukeum <coronalbayes@protonmail.com>
2022-02-13 00:48:18 -0500

    pilot

--- src/train.py
+++ src/train.py
@@ -2,18 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from jiwer import wer,cer
+import numpy as np
+import torch
 
+
+from datasets import load_metric
 from dataclasses import dataclass, field
-from typing import Dict, List, Optional
-from transformers import AdamW
+from typing import Union, Dict, List, Optional
+from transformers import AdamW, AutoTokenizer, T5ForConditionalGeneration, T5Config
 from transformers import (
     HfArgumentParser,
     DataCollator,
-    Trainer,
-    TrainingArguments,
+    Seq2SeqTrainer, 
+    Seq2SeqTrainingArguments,
     set_seed,
 )
 
+import sys
+sys.path.append('src')
+from data_utils import load_pronuncation_dictionary
+
+
+def prepare_dataset(batch):
+    
+    batch['input_ids'] = batch['word']
+    batch['labels'] = batch['pron']
+    
+    return batch
+    
 
 @dataclass
 class DataCollatorWithPadding:
@@ -24,12 +41,12 @@ class DataCollatorWithPadding:
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
         # split inputs and labels since they have to be of different lenghts and need
         # different padding methods
-        inputs = [{"word": feature["word"]} for feature in features]
-        prons = [{"pronunciation": feature["pronunciation"]} for feature in features]
+        words = [feature["input_ids"] for feature in features]
+        prons = [feature["labels"] for feature in features]
 
-        batch = tokenizer(words,padding=padding,add_special_tokens=False,
+        batch = self.tokenizer(words,padding=self.padding,add_special_tokens=False,
                           return_attention_mask=True,return_tensors='pt')
-        pron_batch = tokenizer(prons,padding=padding,add_special_tokens=False,
+        pron_batch = self.tokenizer(prons,padding=self.padding,add_special_tokens=False,
                           return_attention_mask=True,return_tensors='pt')
         
         # replace padding with -100 to ignore loss correctly
@@ -39,41 +56,98 @@ class DataCollatorWithPadding:
         return batch
     
     
-    
-def train(model,dataset,args):
 
-    model.train()
-    
-    losses = []
-    for _ in range(epoch):
-        data = shuffle(data)
-        for i, batch in tqdm(enumerate(batchify(data,batch_size=32))):
-        
-            loss = model(**batch.to(device)).loss
-            
-            loss.backward()
-            if i%args.grad_acc == 0:
-                optimizer.step()
-                optimizer.zero_grad()
-            if i%100 == 0:
-                print("Loss: %s at Iteration: %i"%(loss.item(),i))
-                losses.append(loss.item())
                 
                 
-                
-def evaluate(model,dataset,args):
+def evaluate_all_metrics(model,dataset,args):
     
     model.eval()
     words = tokenizer('힐끔힐끔', return_tensors="pt")
     out = model.generate(**words.to(device),num_beams=5).squeeze()
     tokenizer.decode(out,skip_special_tokens=True)
+    return 
         
+
+
+def compute_metrics(pred):
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions
+
+    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+    labels_ids[labels_ids == -100] = processor.tokenizer.pad_token_id
+    label_str = processor.batch_decode(labels_ids, skip_special_tokens=True)
+
+    cer = cer_metric.compute(predictions=pred_str, references=label_str)
+
+    return {"cer": cer}
+
+
 
 if __name__ == "__main__":
     
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('integers', metavar='N', type=int, nargs='+',
                         help='an integer for the accumulator')
     parser.add_argument('grad_acc', type=int, default=8,
                         help='an integer for the accumulator')    
     args = parser.parse_args()
+    '''
+    
+    cer_metric = load_metric("cer")
+    '''
+    hypotheses = ['ki:m˥','a:m˥']
+    ground_truth = ['hi:m˥','a:m˥']
+    wer(ground_truth,hypotheses)
+    cer(ground_truth,hypotheses)
+    '''
+    
+    
+    
+    data = load_pronuncation_dictionary('other_dicts/fr-fr')
+    data = data.map(prepare_dataset)    
+    train_dataset = data
+    eval_dataset = data
+    
+    config = T5Config.from_pretrained('google/byt5-small')
+    #model = T5ForConditionalGeneration.from_pretrained('google/byt5-small')
+    
+    
+    config
+    config.num_decoder_layers = 2
+    config.num_layers = 8
+    config.d_kv = 32
+    config.d_model = 256
+    config.d_ff = 512
+
+    model = T5ForConditionalGeneration(config)
+    
+    tokenizer = AutoTokenizer.from_pretrained('google/byt5-small')
+
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    
+    training_args = Seq2SeqTrainingArguments(
+        predict_with_generate=True,
+        evaluation_strategy="steps",
+        per_device_train_batch_size=256,
+        per_device_eval_batch_size=512,
+        fp16=False, 
+        output_dir="./results",
+        logging_steps=2,
+        save_steps=1000,
+        eval_steps=1000,
+    )
+    
+    
+    trainer = Seq2SeqTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator=data_collator,
+    )
+    trainer.train()
